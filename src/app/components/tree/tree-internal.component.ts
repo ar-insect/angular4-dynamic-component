cc95abeafa641a805ee1451dad6909ecce7e1112
jianglm <jianglm@microseer.com>
2018-02-07 18:28:15 +0800

    update

--- src/app/components/tree/tree-internal.component.ts
+++ src/app/components/tree/tree-internal.component.ts
@@ -18,27 +18,16 @@ import {
     selector: 'app-tree-internal',
     encapsulation: ViewEncapsulation.None,
     template: `
-        <ul class="tree" *ngIf="treeModel">
-          <ng-container *ngIf="isNode(treeModel)">
-            <li>
-              <div>
-                <span class="icon-node">
-                    <b *ngIf="isexpanded">-</b>
-                    <b *ngIf="!isexpanded">+</b>
-                </span>
-                <span>{{ treeModel.ptext || "root" }}</span>
-              </div>
-              <ng-template [ngIf]="isexpanded">
-                <app-tree *ngFor="let child of treeModel.children" [tree]="child"></app-tree>
-              </ng-template>
-            </li>
-          </ng-container>
-          <ng-container *ngIf="!isNode(treeModel)">
-            <li>
-              <div >{{ treeModel.ptext }}</div>
-            </li>
-          </ng-container>
-      </ul>
+        <ul class="tree" *ngIf="tree">
+          <li>
+          <div>
+            <span (click)="onSwitchFoldingType()">{{ tree.node.ptext || "root" }}</span>
+          </div>
+          <ng-template [ngIf]="tree.isNodeExpanded()">
+            <app-tree-internal *ngFor="let child of tree.children" [tree]="child"></app-tree-internal>
+          </ng-template>
+          </li>
+        </ul>
     `,
     styleUrls: ['./tree.component.scss']
   })
@@ -46,6 +35,10 @@ import {
 
     @Input()
     tree: Tree;
+
+    public onSwitchFoldingType(): void {
+      this.tree.switchFoldingType();
+    }
 
     constructor(
     ) {
--- src/app/components/tree/tree.types.ts
+++ src/app/components/tree/tree.types.ts
@@ -3,4 +3,5 @@ export interface TreeModel {
     id: number;
     ptext: string;
     children?: TreeModel[];
+    _foldingType?: string;
 }
